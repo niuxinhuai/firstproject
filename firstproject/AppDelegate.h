ac08804667225b946aef2c98f6f2ceebeb24d4dc
牛新怀 <niuxinhuai@super-niu.local>
2017-05-09 10:55:28 +0800

    Initial Commit

---
+++ firstproject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstproject
+//
+//  Created by 牛新怀 on 2017/5/9.
+//  Copyright © 2017年 牛新怀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
